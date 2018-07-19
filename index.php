d8c073df62370a35f1105cb91c1c1f6bc5fbffb7
mentosuncle <39184675+mentosuncle@users.noreply.github.com>
2018-07-20 04:19:46 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: http://www.macallisterrentals.com/rental/earthmoving-rental/backhoe-rental/");
+header("Location: https://www.amazon.com/WE-Furniture-Storage-Console-Espresso/dp/B00A7X4M1S/ref=sr_1_20?ie=UTF8&qid=1531593534&sr=8-20&keywords=furniture");
 die();
 ?>
